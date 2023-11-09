d5188299c17f648d9f92fb2bf3a160f3a09aa73d
DSykesTheDev <dezmensykes1@gmail.com>
2023-11-09 00:27:45 -0500

    Updated proper filtering using new Control Flow syntax.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
 import { WishItem } from 'src/shared/models/WishItem';
 
+const wishFilters = [
+  (item:WishItem) => item,
+  (item:WishItem) => item.isComplete(),
+  (item:WishItem) => !item.isComplete()
+]
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  wishText: string = '';
-  wishFilter: string = '0';
+  wishText: string = ''; 
+  wishFilter: any = '0';
 
   items: WishItem[] = [
-    new WishItem("Start this project", ),
+    new WishItem("Start this project"),
     new WishItem("Get First Paycheck", true),
     new WishItem('Start Saving')
   ];
 
-  visibleItems: WishItem[] = this.items;
+  get visibleItems(): WishItem[] {
+    return this.items.filter(wishFilters[this.wishFilter]);
+  };
 
   constructor() {
 
@@ -24,16 +32,6 @@ export class HomeComponent {
 
   toggleItem(item: WishItem) {
     item.Complete();
-  }
-
-  filterChanged(e:any)
-  {
-    if(e === '0')
-      this.visibleItems = this.items
-    else if(e === '1')
-      this.visibleItems = this.items.filter(item => {return item.isComplete() == true})
-    else  
-      this.visibleItems = this.items.filter(item => !item.isComplete())
   }
 
   addWish()
